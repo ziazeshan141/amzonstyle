6e63b1806d55578c01f2e309e6bb5f2455202693
ziazeshan141@outlook.com <ziazeshan141@outlook.com>
2026-08-13 15:29:21 +0300

    upgraded the eks version from 20 to 21

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,10 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 20.0"
+  version = ">= 21.0"
   cluster_name = var.cluster_name
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   eks_managed_node_groups = {
