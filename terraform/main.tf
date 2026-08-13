51658e29d51378febd2173fdafa5bdd3e999af14
ziazeshan141@outlook.com <ziazeshan141@outlook.com>
2026-08-13 15:43:31 +0300

    modified eks section

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,10 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 21.0"
+  version = "~> 21.0"
   name = var.cluster_name
+  kubernetes_version = "1.31"
   endpoint_public_access = true
-  enable_cluster_creator_admin_permissions = true
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   eks_managed_node_groups = {
